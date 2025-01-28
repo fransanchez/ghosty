92df9490adcab4490d6b05bf225d4ee9a42caa53
fransanchez <fransanchezgarcia@gmail.com>
2025-01-28 18:17:08 +0100

    minor refactor of UI images into their own folder

--- include/Utils/Constants.h
+++ include/Utils/Constants.h
@@ -17,7 +17,7 @@ constexpr float JUMP_INITIAL_VELOCITY = -500.0f;
 constexpr float GRAVITY = 981.0f;
 constexpr float RUN_VELOCITY = 1.5f;
 constexpr float WALK_VELOCITY = 1.0f;
-constexpr char* ASSETS_LIVES_PATH = "../data/Images/heart.png";
+constexpr char* ASSETS_LIVES_PATH = "../data/Images/UI/heart.png";
 
 // Enemy Constants
 constexpr float ENEMY_RETURN_POSITION_TOLERANCE = 2.0f;
