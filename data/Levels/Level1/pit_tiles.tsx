745fafbeab12c610dde00a550e2ffdd32333c40b
fransanchez <fransanchezgarcia@gmail.com>
2025-02-02 00:34:48 +0100

    Adding final version of the level

---
+++ data/Levels/Level1/pit_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="pit_tiles" tilewidth="64" tileheight="64" tilecount="14" columns="14">
+ <image source="Tiles/pit_tiles.png" width="896" height="64"/>
+</tileset>
---
+++ data/Levels/Level1/tileset_clear_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="tileset_clear_tiles" tilewidth="64" tileheight="64" tilecount="34" columns="2">
+ <image source="Tiles/tileset_clear_tiles.png" width="128" height="1088"/>
+</tileset>
