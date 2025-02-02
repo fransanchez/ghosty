c71e534539a1d242ea5836f6bdf236f335ac5814
fransanchez <fransanchezgarcia@gmail.com>
2025-02-02 16:27:27 +0100

    Making sure the game loads and works properly with cmake and visual studio builds

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ Ghosty is a platform-style game inspired by Ghosts 'n Goblins, where you control
    cmake --build ../build
 3. (Optional) If you are on Windows, go to scripts folder and run build.bat.
 4. (Optional) If you want to use Visual Studio 2022, run generate.bat from the scripts folder to generarte the solution
+5. (Optional) If you want to see Debug rendering change the target_compile_definitions line with target_compile_definitions(${PROJECT_NAME} PUBLIC DEBUG_MODE=0)
+
 
 ## Features
 
--- include/Utils/Constants.h
+++ include/Utils/Constants.h
@@ -2,28 +2,28 @@
 
 // Game constants
 constexpr char* GAME_TITLE = "Ghosty";
-constexpr char* GAME_CONFIG_PATH = "../data/Config/game_config.json";
-constexpr char* TITLE_SCREEN_IMAGE_PATH = "../data/Images/UI/GhostyMainTitle.png";
-constexpr char* START_BUTTON_IMAGE_PATH = "../data/Images/UI/StartGameButton.png";
-constexpr char* GAME_OVER_SCREEN_IMAGE_PATH = "../data/Images/UI/GameOver.png";
-constexpr char* VICTORY_SCREEN_IMAGE_PATH = "../data/Images/UI/Victory.png";
+constexpr char* GAME_CONFIG_PATH = "./data/Config/game_config.json";
+constexpr char* TITLE_SCREEN_IMAGE_PATH = "./data/Images/UI/GhostyMainTitle.png";
+constexpr char* START_BUTTON_IMAGE_PATH = "./data/Images/UI/StartGameButton.png";
+constexpr char* GAME_OVER_SCREEN_IMAGE_PATH = "./data/Images/UI/GameOver.png";
+constexpr char* VICTORY_SCREEN_IMAGE_PATH = "./data/Images/UI/Victory.png";
 
 // Config Constants
-constexpr char* DINO_ENEMY_CONFIG_PATH = "../data/Config/enemy_config_dino.json";
-constexpr char* GHOST_ENEMY_CONFIG_PATH = "../data/Config/enemy_config_ghost.json";
-constexpr char* LEVEL_MAP_PATH = "../data/Levels/Level1/Level1.tmx";
-constexpr char* PLAYER_CONFIG_PATH = "../data/Config/player_config_mage.json";
-constexpr char* SKELETON_ENEMY_CONFIG_PATH = "../data/Config/enemy_config_skeleton.json";
-constexpr char* VAMPIRE_ENEMY_CONFIG_PATH = "../data/Config/enemy_config_vampire.json";
-constexpr char* COLLECTIBLES_CONFIG_PATH = "../data/Config/collectibles_config.json";
-constexpr char* AUDIO_CONFIG_PATH = "../data/Config/audio_config.json";
+constexpr char* DINO_ENEMY_CONFIG_PATH = "./data/Config/enemy_config_dino.json";
+constexpr char* GHOST_ENEMY_CONFIG_PATH = "./data/Config/enemy_config_ghost.json";
+constexpr char* LEVEL_MAP_PATH = "./data/Levels/Level1/Level1.tmx";
+constexpr char* PLAYER_CONFIG_PATH = "./data/Config/player_config_mage.json";
+constexpr char* SKELETON_ENEMY_CONFIG_PATH = "./data/Config/enemy_config_skeleton.json";
+constexpr char* VAMPIRE_ENEMY_CONFIG_PATH = "./data/Config/enemy_config_vampire.json";
+constexpr char* COLLECTIBLES_CONFIG_PATH = "./data/Config/collectibles_config.json";
+constexpr char* AUDIO_CONFIG_PATH = "./data/Config/audio_config.json";
 
 // Player Constants
 constexpr float JUMP_INITIAL_VELOCITY = -500.0f;
 constexpr float GRAVITY = 981.0f;
 constexpr float RUN_VELOCITY = 1.5f;
 constexpr float WALK_VELOCITY = 1.0f;
-constexpr char* ASSETS_LIVES_PATH = "../data/Images/UI/heart.png";
+constexpr char* ASSETS_LIVES_PATH = "./data/Images/UI/heart.png";
 constexpr float POWER_UP_SECONDS_DURATION = 15.f;
 
 // Enemy Constants
